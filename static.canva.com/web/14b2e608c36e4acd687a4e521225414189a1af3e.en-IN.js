5074124fca06f941256a3414f483b32786dafb1d
Naitik gupta <76028128+Naitik120gupta@users.noreply.github.com>
2021-07-29 22:21:23 +0530

    Add files via upload

---
+++ static.canva.com/web/14b2e608c36e4acd687a4e521225414189a1af3e.en-IN.js
@@ -0,0 +1,7 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["en-IN"] = Object.assign(window['cmsg']['assets']["en-IN"] || {}, {
+    "582": {
+        "js": ["b11f124a3ea9c8c4a8417404caeebbccbcfe2f14.strings.js"],
+        "css": []
+    }
+});
